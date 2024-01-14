18afb23c5909c7520c4ef92ed601d9b261887440
Rachel Barthélery <rachel-barth@live.fr>
2024-01-14 13:30:26 +0100

    MAJ - Page ListEmployee.jsx affiche tabmeau avec les données enregistrées depuis la page CreateEmployee

--- src/pages/ListEmployee.jsx
+++ src/pages/ListEmployee.jsx
@@ -1,64 +1,20 @@
 import '../css/ListEmployee.css';
-import { Link } from 'react-router-dom';
 import { useState, useMemo } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import DataTable from 'react-data-table-component';
 import FilterComponent from '../components/FilterComponent/FilterComponent';
-import data from '../data/testData';
+import CONSTANTS from '../utils/constants';
+
 
 function ListEmployee() {
-    const columns = [
-        {
-            name: 'First Name',
-            selector: row => row.firstname,
-            sortable: true,
-        },
-        {
-            name: 'Last Name',
-            selector: row => row.lastname,
-            sortable: true,
-        },
-        {
-            name: 'Start Date',
-            selector: row => row.startDate,
-            sortable: true,
-        },
-        {
-            name: 'Department',
-            selector: row => row.department,
-            sortable: true,
-        },
-        {
-            name: 'Date of Birth',
-            selector: row => row.dateOfBirth,
-            sortable: true,
-        },
-        {
-            name: 'Street',
-            selector: row => row.street,
-            sortable: true,
-        },
-        {
-            name: 'City',
-            selector: row => row.city,
-            sortable: true,
-        },
-        {
-            name: 'State',
-            selector: row => row.state,
-            sortable: true,
-        },
-        {
-            name: 'Zip Code',
-            selector: row => row.zipCode,
-            sortable: true,
-        },
-    ];
+    const { employeeList } = useSelector((state) => state.save)
 
     const [filterText, setFilterText] = useState('');
 	const [resetPaginationToggle, setResetPaginationToggle] = useState(false);
 
-    const filteredItems = data.filter(
+    const filteredItems = employeeList.filter(
         item => item.firstname && (item.firstname.toLowerCase().includes(filterText.toLowerCase()) || 
                 item.lastname.toLowerCase().includes(filterText.toLowerCase()) || 
                 item.startDate.toLowerCase().includes(filterText.toLowerCase()) || 
@@ -98,7 +54,7 @@ function ListEmployee() {
             </section>
             <section className='employee-list-wrapper'>
                 <DataTable
-                    columns={columns}
+                    columns={CONSTANTS.TABLE_COLUMNS}
                     data={filteredItems}
                     pagination
                     paginationResetDefaultPage={resetPaginationToggle}
